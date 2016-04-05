a873acedf231f7379c8a8175ba7fb2fcc6836086
Mitchell Oleson <moleson21@gmail.com>
2016-04-04 17:37:26 -0700

    started joystick control code

---
+++ Joystick/Saitek_X52.cpp
@@ -0,0 +1,25 @@
+//
+// Saitek_X52.cpp
+// CPP Project
+//
+// Created by Mitchell Oleson on 4/4/2016
+//
+// Written for Windows
+//
+
+#include "Saitek_X52.h"
+
+// Saitek Controller Constructor
+Saitek_X52::Saitek_X52() {
+	;
+}
+
+// Saitek Controller Destructor
+Saitek_X52::~Saitek_X52() {
+	;
+}
+
+// Update button positions and sliders
+void update() {
+	;
+}
---
+++ Joystick/Saitek_X52.h
@@ -0,0 +1,26 @@
+//
+// Saitek_X52.h
+// CPP Project
+//
+// Created by Mitchell Oleson on 4/4/2016
+//
+// Written for Windows
+//
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// Saitek_X52 Controller Class
+class Saitek_X52 {
+	private:
+		// Private Variables
+		// Private Methods
+
+	public:
+		// Public Variables
+		// Public Methods
+		Saitek_X52();
+		~Saitek_X52();
+		void update();
+};
